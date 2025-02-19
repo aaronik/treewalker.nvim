bc9e6555aafd2489eed653bc8a098e5ddd5c83d4
Aaron Sullivan <aasullivan1618@gmail.com>
2025-02-19 08:56:22 -0700

    Add a php test file
    with some tests that I can't quite figure out how to get passing just
    yet. This definitely needs to get done. Is there a missing parser in
    the test runner? I don't see how that would be.

---
+++ tests/treewalker/php_spec.lua
@@ -0,0 +1,19 @@
+local load_fixture = require "tests.load_fixture"
+local tw = require 'treewalker'
+local h = require 'tests.treewalker.helpers'
+
+describe("Movement in an html file", function()
+  before_each(function()
+    load_fixture("/php.php")
+  end)
+
+  -- TODO This also works manually but is failing the test suite. I think
+  -- parsers need to be brought in.
+  pending("respects embedded html", function()
+    vim.fn.cursor(18, 1)
+    tw.move_down()
+    h.assert_cursor_at(19, 1)
+  end)
+
+end)
+
