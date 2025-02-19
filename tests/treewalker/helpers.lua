57c72a469754f588c83e19c66b691aebcfe381e0
Aaron Sullivan <aasullivan1618@gmail.com>
2025-02-19 12:45:46 -0700

    Update helpers.assert_cursor_at to provide more info

--- tests/treewalker/helpers.lua
+++ tests/treewalker/helpers.lua
@@ -1,18 +1,18 @@
 local assert = require('luassert')
+local lines  = require('treewalker.lines')
 
 local M = {}
 
 -- Assert the cursor is in the expected position
----@param row integer
----@param col integer
----@param line string?
-function M.assert_cursor_at(row, col, line)
+---@param expected_row integer
+---@param expected_col integer
+---@param expected_line string?
+function M.assert_cursor_at(expected_row, expected_col, expected_line)
   local cursor_pos = vim.fn.getpos('.')
-  ---@type integer, integer
-  local current_line, current_column
-  current_line, current_column = cursor_pos[2], cursor_pos[3]
-  line = string.format("expected to be at [%s/%s](%s) but wasn't", row, col, line)
-  assert.same({ row, col }, { current_line, current_column }, line)
+  local actual_row, actual_col = cursor_pos[2], cursor_pos[3]
+  local actual_line = lines.get_line(actual_row)
+  local error_line = string.format("expected to be at [%s/%s](%s) but was at [%s/%s](%s)", expected_row, expected_col, expected_line, actual_row, actual_col, actual_line)
+  assert.same({ expected_row, expected_col }, { actual_row, actual_col }, error_line)
 end
 
 -- Feed keys to neovim; keys are pressed no matter what vim mode or state
