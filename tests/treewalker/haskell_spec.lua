7ff3efc7f9e78186e83d3294b6773f951ba78b1d
Aaron Sullivan <aasullivan1618@gmail.com>
2025-04-04 00:03:40 -0700

    Add pending spec for haskell movement regression
    Oh rats!! At some point haskell movement actually just totally broke,
    because it wasn't tested! Bummer. This commit adds a spec for it, but
    does not fix it - the spec is pending. Surprised nobody
    complained.

--- tests/treewalker/haskell_spec.lua
+++ tests/treewalker/haskell_spec.lua
@@ -9,10 +9,23 @@ describe("In a haskell file: ", function()
 
   h.ensure_has_parser("haskell")
 
+  -- Oh dang when did this break?
+  pending("moves around in a haskell file", function ()
+    vim.fn.cursor(1, 1)
+    tw.move_down()
+    h.assert_cursor_at(2, 1)
+    tw.move_down()
+    h.assert_cursor_at(3, 1)
+    tw.move_down()
+    h.assert_cursor_at(6, 1)
+    tw.move_in()
+    h.assert_cursor_at(9, 3)
+  end)
+
   it("moves out of a nested node", function()
     vim.fn.cursor(22, 3)
     tw.move_out()
-    h.assert_cursor_at(19, 1, "|randomList")
+    h.assert_cursor_at(19, 1)
   end)
 end)
 
