7ed37947c927ba0ec542ecb0322c27137ecf09f2
Itzamna <itzamna@mayaprotocol.com>
2025-12-09 13:55:41 -0600

    Add Go test for tab-indented sibling navigation
    Tests the visual column fix by using Go files (which use tabs
    by convention). Validates that move_down/move_up correctly
    navigate between sibling nodes at the same indentation level
    when the file uses tab characters.
    
    Test cases:
    - var declarations as siblings
    - if blocks as siblings
    - top-level function declarations

---
+++ tests/fixtures/go.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	var x = 1
+	var y = 2
+	var z = 3
+
+	if x > 0 {
+		fmt.Println("positive")
+	}
+
+	if y > 0 {
+		fmt.Println("also positive")
+	}
+}
+
+func helper() {
+	var a = 10
+	var b = 20
+}
