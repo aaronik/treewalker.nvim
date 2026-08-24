0f43fdad4ba49bed33c441a07479008707ff45d4
Aaron Sullivan <aasullivan1618@gmail.com>
2026-08-24 11:54:08 -0700

    Fix rust spec spacing

--- tests/treewalker/rust_spec.lua
+++ tests/treewalker/rust_spec.lua
@@ -65,6 +65,7 @@ describe("In a rust file:", function()
     tw.swap_left()
     assert.same('    println!(calculate_area(shape), "shape_area");', lines.get_line(47))
   end)
+
   describe("scope_confined", function()
     before_each(function()
       tw.setup({ scope_confined = true })
